1f2434311a68554c1ea9692385f949d44ca91add
Felipe <felipe-mattioli98@hotmail.com>
2023-02-26 17:24:04 +0000

    FEAT: Add new constant

--- src/Spents.Topics/KafkaTopics.cs
+++ src/Spents.Topics/KafkaTopics.cs
@@ -4,7 +4,15 @@
     {
         public static class Events
         {
+            /// <summary>
+            /// Spents receipts topic
+            /// </summary>
             public const string Receipt = "spents-receipts";
+
+            /// <summary>
+            /// Spents receipts telemetry topic
+            /// </summary>
+            public const string ReceiptTelemetry = "spents-receipts-telemetry";
         }
     }
 }
